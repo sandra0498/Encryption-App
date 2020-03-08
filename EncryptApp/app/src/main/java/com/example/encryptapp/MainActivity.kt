a0756070aebc920d7bebb168b698d5d1732042e5
sandra_c <chavezsandra0498@gmail.com>
2020-03-08 03:38:16 -0700

    Update MainActivity.kt

--- EncryptApp/app/src/main/java/com/example/encryptapp/MainActivity.kt
+++ EncryptApp/app/src/main/java/com/example/encryptapp/MainActivity.kt
@@ -5,35 +5,60 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.util.regex.Pattern
 import kotlin.random.Random
 import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var enterButton : Button
+    lateinit var messageInput : EditText
+    lateinit var  resultsTextView : TextView
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val enterButton = findViewById<Button>(R.id.enterButton)
+        enterButton = findViewById(R.id.enterButton)
 
         // text input for entering the message
-        val messageInput = findViewById<EditText>(R.id.messageInput)
+        messageInput = findViewById(R.id.messageInput)
         //where the results will be displayed
-        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        resultsTextView = findViewById(R.id.resultsTextView)
 
         enterButton.setOnClickListener {
-            //toStrings the actual word input
-            var message = messageInput.text.toString()
-            val rand = (0 until 10).random()
-
-        	// Removes the whitespaces in the word 
-            message = message.replace("\\s".toRegex(),"")
-            
-            /*calls the function to cipher and assigns the returning
-            string to the text view  */
-            resultsTextView.text = cipher(message,rand).toString()
+            checkInput()
         }
+    }
+
+    private fun checkInput(){
+        var message = messageInput.text.toString()
+
+
+
+        if (message.isEmpty() || !ifAlpha(message)){
+            messageInput.error = "Enter only alphabet!"
+            return
+        }
+        val rand = (0 until 10).random()
+
+        // Removes the whitespaces in the word
+        message = message.replace("\\s".toRegex(),"")
+
+        /*calls the function to cipher and assigns the returning
+        string to the text view  */
+        resultsTextView.text = cipher(message,rand).toString()
+
+
+    }
+
+    private fun ifAlpha(text: String) : Boolean{
+        if (Pattern.matches(".*[a-zA-z]+.*",text)){
+            return true
+        }
+        return false
     }
 
     private fun cipher(text: String, shift: Int) : String{
