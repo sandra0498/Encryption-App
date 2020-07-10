e55f0ad7df51a1fe792484d4ea038cd7932cae62
sandra_c <chavezsandra0498@gmail.com>
2020-07-09 21:46:18 -0700

    Update ThirdActivity.kt

--- ThirdActivity.kt
+++ ThirdActivity.kt
@@ -8,5 +8,10 @@ class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        val finalresult = intent.getStringExtra(MainActivity.EXTRA_TEXT)
     }
+        
 }
+    
+    
