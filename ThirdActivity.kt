b3f62b25b883ea7a1d55ae362d1c980443297814
sandra_c <chavezsandra0498@gmail.com>
2020-07-10 21:18:14 -0700

    Update ThirdActivity.kt

--- ThirdActivity.kt
+++ ThirdActivity.kt
@@ -2,16 +2,24 @@ package com.example.encryptapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ThirdActivity : AppCompatActivity() {
+
+    lateinit var conclude : TextView
+    lateinit var displayStr : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
+        conclude = findViewById(R.id.textView)
         val finalresult = intent.getStringExtra(MainActivity.EXTRA_TEXT)
+
+        displayStr = findViewById(R.id.textView2)
+        displayStr.setText(finalresult)
     }
-        
+
 }
-    
-    
+
+
